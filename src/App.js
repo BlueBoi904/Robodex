e86271b35079f27a88647510f426836f587f95d9
Benjamin Mirschel <cliff.mirschel@gmail.com>
2019-05-30 09:33:45 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import CardList from "./CardList";
+import { robots } from './robots';
+import SearchBox from "./SearchBox";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+  //Common error here
+  onSearchChange = event => {
+    this.setState(({searchfield: event.target.value}))
+  }
+
+  render(){
+    const filteredRobots = this.state.robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="tc">
+      <h1 className='f1'>RoboDex</h1>
+      <SearchBox searchChange= {this.onSearchChange}/>
+      <CardList robots={filteredRobots}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Lenny Buckets',
+    username: 'Len720',
+    email: 'lenny@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Trashcan Timmy',
+    username: 'timmyTheMan',
+    email: 'timmy@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'FruityGurl',
+    email: 'grapefruitgurl@att.net'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Lebby Boi',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Stringbean',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mr. Dennis Schulist',
+    username: 'Dennys',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'K Dubs',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'N.R.V',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Rufus Rugburt',
+    username: 'RR',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Boxbrain Berry',
+    username: 'Boxy Boi',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
